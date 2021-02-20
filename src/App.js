ef7970d3a4557e66996d9e8f0a3a5da9e1e874f3
Ayako <usuajiayako@gmail.com>
2021-02-20 19:11:42 +0000

    Made socketio user1 work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import User1 from "./User1";
+
 
 function App() {
   return (
     <div className="App">
+      <User1 />
     </div>
   );
 }
---
+++ src/User1.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000")
+
+class User1 extends Component {
+
+  state = { 
+    chat: [],
+    user: "user1",
+    message: "",
+   }
+ 
+  componentDidMount = () => {
+    socket.on('message', ({ message, user }) => {
+      console.log("after socket on...", message, user)
+      if (this.state.chat[0] !== { message, user })
+      this.setState({ chat:[{ message, user }, ...this.state.chat]})
+    })
+  }
+
+  handleChange = (event) => {
+    console.log("in handleChange...", event.target.value)
+    this.setState({message: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handle submit...", this.state.message)
+    socket.emit("message", { message: this.state.message, user: this.state.user })
+    this.setState({ message: "" })
+  }
+
+  
+  render() { 
+    console.log("render...", this.state.chat)
+    return (
+      <div>
+      <div className="chat-display">
+        {this.state.chat.map(({message, user}) =>
+          <p>{`${message} sentfrom: ${user}`}</p>
+        )}
+      </div>
+      <form onSubmit={this.handleSubmit}>
+      <input type="text" onChange={this.handleChange} value={this.state.message}></input>
+      <button type="submit">send</button>
+      </form>
+      </div> 
+     );
+  }
+}
+ 
+export default User1;
